e2d862ed560555b1399b91078fa132457d13f187
Ajinkyad2101 <74656150+Ajinkyad2101@users.noreply.github.com>
2021-05-17 21:30:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "CONSTANT";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("S T A N C O N T", 100,50)
+    fill("lightblue")
+    text("Hint: Always fixed, never changing !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("U C T I F O N", 700,50)
+    fill("lightblue")
+    text("Hint: used to perform certain tasks ?!", 700,70)
+
+    fill("white")
+    textSize(15)
+    text("A B S A D E T A", 100,300)
+    fill("lightblue")
+    text("Hint: Always fixed, never changing !!", 100,320)
+
+
+    
+
+}
